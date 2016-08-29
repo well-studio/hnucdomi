8931a04ad3d629e82f4b9b7383bb697850a1cee6
zzliux <416918875@qq.com>
2016-08-29 11:10:52 +0800

    重写了跳转页面

--- application/config.php
+++ application/config.php
@@ -133,8 +133,8 @@ return [
     // 视图输出字符串内容替换
     'view_replace_str'       => [],
     // 默认跳转页面对应的模板文件
-    'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
-    'dispatch_error_tmpl'    => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
+    'dispatch_success_tmpl'  => THINK_PATH . '../config/tpl/jump.tpl',
+    'dispatch_error_tmpl'    => THINK_PATH . '../config/tpl/jump.tpl',
 
     // +----------------------------------------------------------------------
     // | 异常及错误设置
